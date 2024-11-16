1c60472e571907ca05b8153796e2a421d3e71f2f
Pedro Gardim <me@pedrogardim.com>
2024-11-16 13:03:38 +0100

    feature: map focus on mission

--- sections/mission/components/MissionMap/MissionMap.tsx
+++ sections/mission/components/MissionMap/MissionMap.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 import { useExploreMissions } from "@/mission/application/useExploreMissions";
@@ -7,26 +8,48 @@ import { mapSettings } from "./settings";
 import { Marker } from "./partials/Marker";
 
 export const MissionMap = () => {
+  const pathname = usePathname();
   const { missions, refetch } = useExploreMissions();
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyCcY7g-HEkrTVHkuj5MvNDRPQNGeQF8CzA",
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
 
-  const onLoad = React.useCallback((map: any) => {
+  const onLoad = React.useCallback((map: google.maps.Map) => {
     setMap(map);
     refetch();
   }, []);
 
-  const onUnmount = React.useCallback((map: any) => {
+  const onUnmount = React.useCallback((map: google.maps.Map) => {
     setMap(null);
   }, []);
+
+  const onRouteChange = React.useCallback(
+    (pathname: string) => {
+      if (!map) return;
+      const missionPrefix = "/mission/";
+      if (!pathname.startsWith(missionPrefix)) return;
+
+      const id = pathname.replace(missionPrefix, "");
+      const mission = missions?.find((mission) => mission.id === parseInt(id));
+      if (!mission) return;
+
+      const { lat, long } = mission;
+      map.setCenter({ lat: lat as number, lng: long as number });
+      map.setZoom(16);
+    },
+    [map, missions]
+  );
 
   useEffect(() => {
     refetch();
   }, []);
+
+  useEffect(() => {
+    onRouteChange(pathname);
+  }, [pathname, missions, map]);
 
   return isLoaded ? (
     <GoogleMap
@@ -37,8 +60,6 @@ export const MissionMap = () => {
         streetView: null,
       }}
       mapContainerStyle={{ height: "100vh", width: "100%" }}
-      center={{ lat: 0, lng: 0 }}
-      zoom={3}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
