416263b801516fd5443b39e08a70b71ad15aaceb
Pedro Gardim <me@pedrogardim.com>
2024-11-26 20:05:27 +0100

    feature: add mission summary component

--- sections/mission/components/MissionCreationForm/MissionCreationForm.tsx
+++ sections/mission/components/MissionCreationForm/MissionCreationForm.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 
 import { Form } from "@/components/ui/form";
 import {
@@ -13,26 +15,55 @@ import {
 import { useMissionCreation } from "@/mission/application/useMissionCreation";
 import explore from "@/assets/images/illustration/explore.png";
 import { Button } from "@/components/ui/button";
+import { Summary } from "./partials/summary";
 
-export const MissionCreationForm = ({}) => {
+export const MissionCreationForm = () => {
   const t = useTranslations("action");
-  const { form } = useMissionCreation({ onSuccess: () => {} });
+  const router = useRouter();
+  const [summaryOpen, setSummaryOpen] = useState(false);
+  const { form, onSubmit } = useMissionCreation({
+    onSuccess: () => {
+      router.replace("/missions/my-missions");
+      router.refresh();
+    },
+  });
+
+  const onNextButtonClick = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    summaryOpen ? onSubmit() : setSummaryOpen(true);
+  };
 
   return (
-    <Form {...form}>
-      <form className="max-w-screen-md mx-auto">
-        <Image className="mx-auto" src={explore} alt="Create a mission" width={400} height={400} />
-        <div className="flex flex-col gap-4 gap-y-8">
-          <DetailsSection form={form} />
-          <DurationSection form={form} />
-          <CategorySection form={form} />
-          <LocationSection form={form} />
-          <ImageSection form={form} />
-          <Button className="ml-auto mt-8" size="lg">
-            {t("next")}
-          </Button>
-        </div>
-      </form>
-    </Form>
+    <>
+      <Form {...form}>
+        <form className="max-w-screen-md mx-auto">
+          {!summaryOpen && (
+            <Image
+              className="mx-auto"
+              src={explore}
+              alt="Create a mission"
+              width={400}
+              height={400}
+            />
+          )}
+          <div className="flex flex-col gap-4 gap-y-8">
+            {!summaryOpen && (
+              <>
+                <DetailsSection form={form} />
+                <DurationSection form={form} />
+                <CategorySection form={form} />
+                <LocationSection form={form} />
+                <ImageSection form={form} />
+              </>
+            )}
+            {summaryOpen && <Summary form={form} />}
+            <Button className="ml-auto mt-8" size="lg" onClick={onNextButtonClick}>
+              {t("next")}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
   );
 };
