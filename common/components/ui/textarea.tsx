e555157ed61e7f56d077ea0290c77ab88297de51
Pedro Gardim <me@pedrogardim.com>
2024-12-07 13:43:23 +0100

    style fix

--- common/components/ui/textarea.tsx
+++ common/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<"tex
           "flex min-h-[80px] w-full rounded-md max-h-[200px] ",
           "border border-neutral-400 bg-white px-3 py-2",
           "text-base placeholder:text-neutral-500",
-          "focus-visible:border-neutral-500 focus-visible:shadow-sm focus-visible:outline-none",
+          "focus-visible:border-indigo-500 focus-visible:border-2  focus-visible:shadow-sm focus-visible:outline-none",
           "disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "dark:border-neutral-800 dark:bg-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
           className
